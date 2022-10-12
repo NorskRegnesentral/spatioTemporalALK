ff3866319ae92da73d2edd4e585c4ddec9b5d1ee
Olav Nikolai Breivik <olavbr@nr.no>
2022-10-12 09:36:46 +0200

    minor typo in starting value

--- R/par.R
+++ R/par.R
@@ -14,7 +14,7 @@ defpar_alk = function(data, conf){
   par = list(beta0_alk = array(0,dim=c(length(conf$years),nAge-1)),
              log_sigma_beta0_alk = 3,
              betaLength_alk = rep(0,nAge-1),
-             logSigma_alk = c(-2,2),
+             logSigma_alk = c(2,2),
              logKappa_alk = c(-4,-4),
              transRho_alk = 0,
              xS_alk = xS_alk,
