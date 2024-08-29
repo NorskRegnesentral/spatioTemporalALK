9bea49eac6d06cfde462f03efa68dc90dba191a2
Olav Nikolai Risdal Breivik <olavbr@nr.no>
2024-08-29 10:59:41 +0200

    example and readme

--- script/fitALK.R
+++ script/fitALK.R
@@ -2,8 +2,7 @@ library(spatioTemporalALK)
 rm(list=ls())
 
 #Read raw data
-dataAge = readRDS("data/catch_at_age_data_ex_rus.rds")
-#dataAge = dataAge[order(dataAge$startdatetime,dataAge$age),]
+dataAge = readRDS("data/catch_at_age.rds")
 
 #Define configurations
 conf = defConf_alk(years = 2018:2020,
@@ -12,7 +11,7 @@ conf = defConf_alk(years = 2018:2020,
                    spatioTemporal = 0,
                    spatial = 0,
                    betaLength = 1,
-                   cutoff =80, cbound = 130, 
+                   cutoff =40, cbound = 130, 
                    rwBeta0 = 1,
                    readability = 0)
 
@@ -28,6 +27,7 @@ run = fitALK(data,par,conf)
 endTime = Sys.time()
 endTime-startTime
 
+#Plot ALK with spatial effect set to zero
 plotALK(run,year = 2019)
 
 #OSA residuals
@@ -41,42 +41,6 @@ res <- oneStepPredict(run$obj, observation.name ="age",
                       conditional = conditional)
 plot(res$residual)
 
-
-
-
-#Example of plot of ALK wihthout spatio-temporal effect
-x11(width = 20, height = 15)
-par(oma=c(3,3,2,0.5),mar=c(2,2,2,0.5),mfrow = c(5,6))
-for(year in 1994:2020){
-  plotALK(run,year = year)
-}
-
-
-#Example of plot of ALK with spatial effect
-x11()
-year = 2019
-age = 4
-par(mfrow = c(3,2))
-for(length in c(30,40,45,50,55,60)){
-  plotALK(run,year = year,length = length,age = age)
-}
-
-
-
-
-
-
-
-#Go a newton step
-he <- function(par){ optimHess(par, run$obj$fn, run$obj$gr) }
-newtonsteps = 2
-for(i in seq_len(newtonsteps)) { 
-  g <- as.numeric( run$obj$gr(run$opt$par) )
-  h <- stats::optimHess(run$opt$par, run$obj$fn, run$obj$gr)
-  run$opt$par <- run$opt$par- solve(h, g)
-  run$opt$objective <- run$obj$fn(run$opt$par)
-}
-run$opt$he <- optimHess(run$opt$par, run$obj$fn, run$obj$gr)
 
 
 
